91004377c27a6e3073ca059aee13b8b2d451fa3b
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-11-03 23:15:50 -0800

    L16 Dialog Button Added

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 // todo
+import "./style.css";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const canvas = document.getElementById("canvas");
+const _ctx = canvas?.getContext("2d");
+
+const alertButton = document.createElement("button");
+alertButton.innerHTML = "Click Me";
+app.append(alertButton);
+
+alertButton.onclick = () => {
+  alert("You've clicked the button!");
+};
