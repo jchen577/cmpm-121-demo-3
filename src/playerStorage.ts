636d6ba1002a93f6132f1a5fefeca0c1700998ea
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-12-02 12:24:48 -0800

    bufix

--- src/playerStorage.ts
+++ src/playerStorage.ts
@@ -1,10 +1,12 @@
+import leaflet from "leaflet";
 export class PlayerStorage {
   static getPlayerPosition() {
     const savedPosition = localStorage.getItem("playerPosition");
     if (savedPosition) {
       return JSON.parse(savedPosition);
+    } else {
+      return leaflet.latLng(37.793439, -122.410296);
     }
-    return null;
   }
 
   static savePlayerPosition(lat: number, lng: number) {
