31f1f56edcd19ea2710e0782717efdd304ff0be3
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-12-02 12:33:47 -0800

    magically working?

--- src/playerStorage.ts
+++ src/playerStorage.ts
@@ -1,8 +1,6 @@
 export class PlayerStorage {
   static getPlayerPosition() {
     const savedPosition = localStorage.getItem("playerPosition");
-    console.log(JSON.parse(savedPosition));
-    console.log({ lat: 37.793439, lng: -122.410296 });
     if (savedPosition) {
       return JSON.parse(savedPosition);
     } else {
